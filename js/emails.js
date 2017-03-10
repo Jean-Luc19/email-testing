71726f52452f643ff2cbbfa27a8d63edf3fcecee
Aaron Gottlieb <aticus19@gmail.com>
2017-03-10 13:26:19 -0700

    adds more emails to the database

--- js/emails.js
+++ js/emails.js
@@ -14,6 +14,55 @@ export default
             to: "TeamWoz@Woz.org",
             title: "Do you know PHP?",
             content: "Dear Woz.  We are in need of a PHP expert.  Fast.  Zuck x"
+        },
+        2: {
+            id: 2,
+            from: "zuck@twitter.com",
+            to: "TeamWoz@Woz.org",
+            title: "Do you know PHP?",
+            content: "Dear Woz.  We are in need of a PHP expert.  Fast.  Zuck x"
+        },
+        3: {
+            id: 3,
+            from: "zuck@trello.com",
+            to: "TeamWoz@Woz.org",
+            title: "Do you know PHP?",
+            content: "Dear Woz.  We are in need of a PHP expert.  Fast.  Zuck x"
+        },
+        4: {
+            id: 4,
+            from: "zuck@instagram.com",
+            to: "TeamWoz@Woz.org",
+            title: "Do you know PHP?",
+            content: "Dear Woz.  We are in need of a PHP expert.  Fast.  Zuck x"
+        },
+        5: {
+            id: 5,
+            from: "zuck@thinkful.com",
+            to: "TeamWoz@Woz.org",
+            title: "Do you know PHP?",
+            content: "Dear Woz.  We are in need of a PHP expert.  Fast.  Zuck x"
+        },
+        6: {
+            id: 6,
+            from: "zuck@tsnapchat.com",
+            to: "TeamWoz@Woz.org",
+            title: "Do you know PHP?",
+            content: "Dear Woz.  We are in need of a PHP expert.  Fast.  Zuck x"
+        },
+        7: {
+            id: 7,
+            from: "zuck@myspace.com",
+            to: "TeamWoz@Woz.org",
+            title: "Do you know PHP?",
+            content: "Dear Woz.  We are in need of a PHP expert.  Fast.  Zuck x"
+        },
+        8: {
+            id: 8,
+            from: "zuck@square-space.com",
+            to: "TeamWoz@Woz.org",
+            title: "Do you know PHP?",
+            content: "Dear Woz.  We are in need of a PHP expert.  Fast.  Zuck x"
         }
     },
     spam: {
